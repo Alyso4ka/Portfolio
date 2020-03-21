0e007d3fdca5c5dd4e56f3bcffaf8d00ffca0c06
Alyaso4ka <minety1997@mail.ru>
2020-03-21 14:16:16 +0300

    1 block completed

---
+++ src/Skills/Skill/Skill.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Skill.module.css';
+
+function Skill() {
+    return (
+
+        <div className={styles.skill}>
+            <img src="https://klike.net/uploads/posts/2019-03/1551511784_4.jpg"/>
+            <h2>React</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at consequuntur deserunt earum eligendi enim facilis id illum ipsam modi molestias officia omnis, praesentium quasi quod, repellat voluptatibus. Accusantium, aperiam?</p>
+        </div>
+    );
+}
+
+export default Skill;
--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import styles from './Skills.module.css';
+import Skill from './Skill/Skill';
 
-function Skills () {
-  return (
-    <div className={styles.skills}>
+function Skills() {
+    return (
+        <div className={styles.skills}>
 
-      
-    </div>
-  );
+            <div className={styles.container}>
+                Мои скиллы
+                <div className={styles.allSkill}>
+
+                    <Skill />
+                    <Skill/>
+                    <Skill/>
+
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Skills;
