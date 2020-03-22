c3b0c307eb3cb38b60bc63f3b5fed55598436a44
Alyaso4ka <minety1997@mail.ru>
2020-03-22 14:47:43 +0300

    2 block completed

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import styles from './Projects.module.css';
+import Works from "./Works/Works";
 
-function Projects () {
-  return (
-    <div className={styles.projects}>
+function Projects() {
+    return (
+        <div className={styles.projects}>
+            <div className={styles.container}>
+                Мои работы
+                <div className={styles.allWorks}>
 
-      
-    </div>
-  );
+                    <Works/>
+                    <Works/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Projects;
---
+++ src/Projects/Works/Works.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Works.module.css';
+
+function Works() {
+    return (
+
+        <div className={styles.works}>
+<img src='https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/88138269_200764577797901_8209869720002638007_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=y9hYa3-emagAX9fS7WI&oh=7d80c937133d3805a23977612f72ae12&oe=5E966FC7'/>
+
+            <h2>Мои работы</h2>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at dignissimos dolorem eius, ipsum necessitatibus nulla veritatis. A, est explicabo magni minima nesciunt obcaecati odit pariatur quasi sapiente unde voluptatem?
+
+        </div>
+    );
+}
+
+export default Works;
