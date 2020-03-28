12d95c1da92d2f47b7f027538558f38f87619214
Alyaso4ka <minety1997@mail.ru>
2020-03-28 14:54:25 +0300

    5 block completed

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import styles from './Contacts.module.css';
 
-function Contacts () {
-  return (
-    <div className={styles.contacts}>
+function Contacts() {
+    return (
+        <div className={styles.contacts}>
 
-      
-    </div>
-  );
+            <div className={styles.container}>
+                <h2>Контакты</h2>
+                <form action="#">
+                    <div><input></input></div>
+                    <div>  <input></input></div>
+                    <div> <textarea></textarea></div>
+                    <div>  <button>Отправить</button> </div>
+                </form>
+            </div>
+
+        </div>
+    );
 }
 
 export default Contacts;
