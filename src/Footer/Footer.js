a89d61848894e07b52f0aeb0a7ee5ae0bc79e630
Alyaso4ka <minety1997@mail.ru>
2020-03-29 12:13:50 +0300

    6 block completed

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,7 +4,11 @@ import styles from './Footer.module.css';
 function Footer () {
   return (
     <div className={styles.footer}>
-
+      <div className={styles.container}>
+       <h2>Зинюк Евгений</h2>
+        <div>******************</div>
+        <h4>© 2020 Все права защищены</h4>
+      </div>
       
     </div>
   );
