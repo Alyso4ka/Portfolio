469eb42de78888595b0c92107b47b23be42b0034
Alyaso4ka <minety1997@mail.ru>
2020-04-02 21:37:33 +0300

    Updates

---
+++ precache-manifest.2c62d2576673669d30e8040ac2ac557f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6472c6d2e875debfc59cb3c30eb01ed0",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "6f04cd8ddf48316711ec",
+    "url": "/Portfolio/static/css/main.8f2b70e4.chunk.css"
+  },
+  {
+    "revision": "802b15076f7554b5ff97",
+    "url": "/Portfolio/static/js/2.ef185dab.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Portfolio/static/js/2.ef185dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f04cd8ddf48316711ec",
+    "url": "/Portfolio/static/js/main.dda919f7.chunk.js"
+  },
+  {
+    "revision": "b56498811c90b2f2740c",
+    "url": "/Portfolio/static/js/runtime-main.d38ab006.js"
+  }
+]);
