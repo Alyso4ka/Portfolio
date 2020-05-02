e54c7449cf9dd80b2d33d470dc381935b628961f
Женя <minety1997@mail.ru>
2020-05-02 12:04:39 +0300

    UPDATE ICONS

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -2,31 +2,31 @@ import React from 'react';
 import styles from './Skills.module.css';
 import Skill from './Skill/Skill';
 import {faCss3, faHtml5, faJs, faReact} from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope} from "@fortawesome/free-regular-svg-icons";
 
 
 
 class Skills extends React.Component {
 
     state = {
-        faItem: [
-            {icon: faJs, className:styles.item, size: "5x", name: "JAVASCRIPT"},
-            {icon: faReact, className:styles.item, size: "5x", name: "REACT"},
-            {icon: faReact, className:styles.item, size: "5x", name: "REDUX"},
-            {icon: faHtml5, className:styles.item, size: "5x", name: "HTML"},
-            {icon: faCss3, className:styles.item, size: "5x", name: "CSS"},
+        iconItem: [
+            {icon: faReact, className:styles.item, size: "4x", name: "REACT"},
+            {icon: faJs, className:styles.item, size: "4x", name: "JAVASCRIPT"},
+            {icon: faHtml5, className:styles.item, size: "4x", name: "HTML"},
+            {icon: faCss3, className:styles.item, size: "4x", name: "CSS"},
         ]
     }
 
 
     render() {
 
-        let  SkillItem = this.state.faItem.map(it => <Skill name={it.name} className={it.className} icon={it.icon} size={it.size}/>  )
+        let  SkillItem = this.state.iconItem.map(i => <Skill icon={i.icon} className={i.className} size={i.size} name={i.name}/>  )
 
         return (
             <div className={styles.skills}>
                 <div className={styles.container}>
                     <div className={styles.mySkills}>
-                        <h2 className={styles.head}> Мои <span> скилы</span> </h2>
+                        <h2 className={styles.head}> Мои <span> скиллы</span> </h2>
                         <div className={styles.line}></div>
                     </div>
                     <div className={styles.allSkill}>
